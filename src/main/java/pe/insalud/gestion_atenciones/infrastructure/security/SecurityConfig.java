e07d6d8285f0e40b35a8cfb7875a49532dd4fe95
AlexlmL <alexcantsed@hotmail.com>
2025-09-17 22:19:06 -0500

    feat Front insertado
    TODO: Comentarios del código del backend y quizá frontend

--- src/main/java/pe/insalud/gestion_atenciones/infrastructure/security/SecurityConfig.java
+++ src/main/java/pe/insalud/gestion_atenciones/infrastructure/security/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfig = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfig.setAllowedOrigins(List.of("http://localhost:5173"));
+                    corsConfig.setAllowedOriginPatterns(List.of("http://localhost:*"));
                     corsConfig.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
                     corsConfig.setAllowedHeaders(List.of("*"));
                     return corsConfig;
